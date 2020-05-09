853b816431281a2ad965f4e98cb92a255ae296da
Edvin2002 <55025658+Edvin2002@users.noreply.github.com>
2020-05-09 21:43:37 +0200

    speed 5 is now a bit slower than speed 4 for 39 speed
    dunno if it causes problems in mp if you modify speed 5 thats why i have choosen this order

--- common/defines/hfu_defines.lua
+++ common/defines/hfu_defines.lua
@@ -13,7 +13,8 @@ NDefines.NTechnology.MAX_SUBTECHS = 4
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 6
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 300
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 100
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.35, 0.15, 0.1, 0.035, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.2, 0.035, 0.08 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+ -- game speeds for each level. Must be 5 entries with last one 0 for unbound
 
 
 NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
